f25595c62fb06782d25c3331eafebe50e2ed453f
Bruce Harris <bruceharris91@gmail.com>
2018-03-15 10:19:34 -0500

    update

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -12,5 +12,49 @@ router.get('/me', tokenMiddleware, isLoggedIn, (req, res) => {
     res.json(req.user);
 });
 
+router.get('/', (req, res) => {
+    console.log('get all users (this is commented out)');
+    classTable.getAll()
+    .then((results) => {
+        res.json(results);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
+router.get('/:id?', (req, res) => {
+    console.log('get 1 user')
+    classTable.getOne(req.params.id)
+    .then((results) => {
+        res.json(results);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
+
+router.put('/:id', (req, res) => {
+    classTable.update(req.params.id, req.body)
+    .then((results) => {
+        res.json(results);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    classTable.delete(req.params.id)
+    .then((results) => {
+        res.json(results);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
+
 
 export default router;
